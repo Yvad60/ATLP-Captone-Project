de51ce2c9dcedb16f6f7d578fe8ed8741d0d00f5
Yvad <yvesyvad@gmail.com>
2021-12-15 16:00:04 +0200

    adding the Full javascript Functionality

--- firebase-init.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBbx7fohOXdPaZKZ0xLKsQtRSe9NEto8T0",
-  authDomain: "atlp-capstone-a3cf7.firebaseapp.com",
-  projectId: "atlp-capstone-a3cf7",
-  storageBucket: "atlp-capstone-a3cf7.appspot.com",
-  messagingSenderId: "792936687876",
-  appId: "1:792936687876:web:8c4077a974c6537dfdc851",
-};
