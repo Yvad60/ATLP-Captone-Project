cad601f5c53eeb8819c65f1d92ae887f0f30f71a
Yvad <yvesyvad@gmail.com>
2022-01-29 23:14:38 +0200

    fix contact us form

--- landing page/landing-script.js
+++ landing page/landing-script.js
@@ -1,38 +1,38 @@
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", submitToFirebase);
-
-function submitToFirebase(e) {
-  e.preventDefault();
+const contactForm = document.getElementById('contactForm')
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const formData = new FormData(e.target)
+  const formEntries = Object.fromEntries(formData.entries())
+  const formInputJSON = JSON.stringify(formEntries)
+  sendMessage(formInputJSON)
   Swal.fire({
-    title: 'Error!',
-    text: 'Do you want to continue',
-    icon: 'error',
-    confirmButtonText: 'Cool'
+    icon: 'success',
+    title: 'Sent!',
+    text: 'Your message is sent'
   })
+  contactForm.reset()
+})
 
-  // // storing form value
-  // let enteredName = getInputValue("name");
-  // let enteredEmail = getInputValue("email");
-  // let enteredMessage = getInputValue("message");
 
-  // if (isFormDataValid(enteredName, enteredEmail)) {
-  //   saveMessage(enteredName, enteredEmail, enteredMessage);
-  //   alert("Message Sent")
-  //   document.getElementById("contactForm").submit();
-  // }
-}
+// if (isFormDataValid(enteredName, enteredEmail)) {
+//   saveMessage(enteredName, enteredEmail, enteredMessage);
+//   alert("Message Sent")
+//   document.getElementById("contactForm").submit();
+// }
 
 
 // //reference messages collection
 // let messagesRef = appDatabase.ref("Messages");
 
-// //function to save message
-// function saveMessage(name, email, message) {
-//   let newMessage = messagesRef.push();
-//   newMessage.set({
-//     userName: name,
-//     userEmail: email,
-//     userMessage: message,
-//   });
-// }
+const sendMessage = async (data) => {
+  const apiEndpoint = 'http://localhost:5000/api/v1/messages'
+  const response = await fetch(apiEndpoint, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: data
+  })
+  const responseData = response.json()
+  console.log(responseData);
+}
